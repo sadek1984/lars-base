ee5618cc124deced204f44e220de687be1977b87
Ahmed Sadek <asadek979@gmail.com>
2026-05-31 23:25:09 +0300

    feat: update dataset path and new excel file for may sprint

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,3 +1,3 @@
 from .enums.ResponseEnums import ResponseSignal
 from .enums.ProcessingEnum import ProcessingEnum
-from .PesticideClassificationModel import PesticideClassificationModel
+# from .PesticideClassificationModel import PesticideClassificationModel
